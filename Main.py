176c4653341d34df757a874dba9552d996cec940
AndrewYing <andrewying@microsoft.com>
2022-07-20 14:57:22 +0800

    Fix1

--- Main.py
+++ Main.py
@@ -2,8 +2,10 @@ import Server.MServer as MS
 import Client.MClient as MC
 
 def main():
-  ms = MS()
-  mc = MC()
+  ms = MS.MServer()
+  mc = MC.MClient()
+  print(ms.stest())
+  print(mc.test())
   
 
 if __name__=="__main__":
